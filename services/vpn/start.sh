6c21e7d163673d8ebdc803a132e0c5f79d3e9faf
bryu <bryu>
2026-08-19 15:55:47 +0400

    Issue OpenVPN certificates with proper EKU

--- services/vpn/start.sh
+++ services/vpn/start.sh
@@ -11,13 +11,32 @@ install -d -m 0700 "$vpn_dir"
 if [[ ! -s "$vpn_dir/ca.crt" ]]; then
   openssl req -x509 -newkey rsa:3072 -nodes -days 3650 -subj '/CN=Bank Lab VPN CA' \
     -keyout "$vpn_dir/ca.key" -out "$vpn_dir/ca.crt"
-  openssl req -newkey rsa:3072 -nodes -subj '/CN=bank_vpn' -keyout "$vpn_dir/server.key" -out "$vpn_dir/server.csr"
-  openssl x509 -req -days 1825 -in "$vpn_dir/server.csr" -CA "$vpn_dir/ca.crt" -CAkey "$vpn_dir/ca.key" \
-    -CAcreateserial -out "$vpn_dir/server.crt"
-  openssl req -newkey rsa:3072 -nodes -subj '/CN=banklab-client' -keyout "$vpn_dir/client.key" -out "$vpn_dir/client.csr"
-  openssl x509 -req -days 1825 -in "$vpn_dir/client.csr" -CA "$vpn_dir/ca.crt" -CAkey "$vpn_dir/ca.key" \
-    -CAcreateserial -out "$vpn_dir/client.crt"
+fi
+if [[ ! -s "$vpn_dir/tls-crypt.key" ]]; then
   openvpn --genkey secret "$vpn_dir/tls-crypt.key"
+fi
+
+cert_has_eku() {
+  [[ -s "$1" ]] && [[ "$(openssl x509 -in "$1" -noout -text 2>/dev/null)" == *"$2"* ]]
+}
+
+refresh_client=0
+if ! cert_has_eku "$vpn_dir/server.crt" 'TLS Web Server Authentication'; then
+  [[ -s "$vpn_dir/server.key" ]] || openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:3072 -out "$vpn_dir/server.key"
+  openssl req -new -key "$vpn_dir/server.key" -subj '/CN=bank_vpn' -out "$vpn_dir/server.csr"
+  openssl x509 -req -days 1825 -in "$vpn_dir/server.csr" -CA "$vpn_dir/ca.crt" -CAkey "$vpn_dir/ca.key" \
+    -CAcreateserial -out "$vpn_dir/server.crt" -extfile <(printf '%s\n' \
+      'basicConstraints=CA:FALSE' 'keyUsage=digitalSignature,keyEncipherment' \
+      'extendedKeyUsage=serverAuth' 'subjectAltName=DNS:bank_vpn')
+fi
+if ! cert_has_eku "$vpn_dir/client.crt" 'TLS Web Client Authentication'; then
+  [[ -s "$vpn_dir/client.key" ]] || openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:3072 -out "$vpn_dir/client.key"
+  openssl req -new -key "$vpn_dir/client.key" -subj '/CN=banklab-client' -out "$vpn_dir/client.csr"
+  openssl x509 -req -days 1825 -in "$vpn_dir/client.csr" -CA "$vpn_dir/ca.crt" -CAkey "$vpn_dir/ca.key" \
+    -CAcreateserial -out "$vpn_dir/client.crt" -extfile <(printf '%s\n' \
+      'basicConstraints=CA:FALSE' 'keyUsage=digitalSignature,keyEncipherment' \
+      'extendedKeyUsage=clientAuth')
+  refresh_client=1
 fi
 
 read -r vpn_network vpn_netmask < <(python3 - "$VPN_POOL" <<'PY'
@@ -32,7 +51,7 @@ if [[ ! -s "$vpn_dir/server.conf" ]]; then
   envsubst </defaults/server.conf.template >"$vpn_dir/server.conf"
 fi
 
-if [[ ! -s "$vpn_dir/client.ovpn" ]]; then
+if [[ ! -s "$vpn_dir/client.ovpn" || "$refresh_client" = 1 ]]; then
   cat >"$vpn_dir/client.ovpn" <<EOF
 client
 dev tun
